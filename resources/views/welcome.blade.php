5d7eba005b8c3356ba63254ea5e84e31b4ba1abe
A <imalexrd@gmail.com>
2025-08-03 02:11:32 -0600

    socialite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 dark:bg-gray-900">
     <div class="flex items-center justify-center min-h-screen">
